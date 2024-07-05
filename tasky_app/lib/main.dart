9c5c3e6018d3767b9735faf0abce5399b080c871
onishi taisei <taisei.developer@gmail.com>
2024-07-06 02:57:47 +0900

    🚀 add: Dartコード内で使用する環境変数を設定

--- tasky_app/lib/main.dart
+++ tasky_app/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'config/config.dart';
+
 void main() {
+  final config = Config.current;
+  debugPrint(config.baseUrl);
   runApp(const MyApp());
 }
 
