d8feb13db25778242f4c58565467856bcdea4466
onishi taisei <taisei.developer@gmail.com>
2024-07-07 19:57:31 +0900

    🚀 add: dockerでローカルDB立ち上げてusersテーブルを操作できるよう実装

--- tasky_server/config/config.go
+++ tasky_server/config/config.go
@@ -2,13 +2,22 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func InitDB() *sql.DB {
-	db, err := sql.Open("mysql", "user:password@tcp(db:3306)/taskydb")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
--- tasky_server/models/user.go
+++ tasky_server/models/user.go
@@ -1,7 +1,10 @@
 package models
 
 type User struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	ID        int    `json:"id" db:"id"`
+	Name      string `json:"name" db:"name"`
+	Email     string `json:"email" db:"email"`
+	Password  string `json:"password" db:"password"`
+	CreatedAt string `json:"created_at" db:"created_at"`
+	UpdatedAt string `json:"updated_at" db:"updated_at"`
 }
